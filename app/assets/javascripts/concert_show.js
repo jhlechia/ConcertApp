ea764d868bc20affce6f10b3f6b6aa9556d817b7
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-06 16:02:47 -0500

    Added turbolinks to tr's on User Profile page and cleaned up unneccessary buttons

--- app/assets/javascripts/concert_show.js
+++ app/assets/javascripts/concert_show.js
@@ -1,11 +1,6 @@
+// 
 // $(document).on('page-change', function(){
-//   $('tr[data-link]').click(function(){
-//     window.location = this.dataset.link;
+//   $("tr[data-link]").click(function(){
+//   window.location = this.dataset.link;
 //   });
 // });
-
-$(document).on('page-change', function(){
-  $('tr[data-link]').click(function(){
-    window.location = this.data("link");
-  });
-});

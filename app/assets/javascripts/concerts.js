e1be506fa87106210d41f31d112feaf74a8706b3
marcemc <marcemc@yahoo.com>
2016-03-06 17:22:18 -0500

    Added 'common' folder in views with 'spinner' partial. Also spinner js into concert.js not finished.

--- app/assets/javascripts/concerts.js
+++ app/assets/javascripts/concerts.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var init_stock_lookup;
+init_stock_lookup = function(){
+  $('#stock-lookup-form').on('ajax:before',function(){
+    show_spinner();
+  });
+
+
+}
--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,14 +2,14 @@ class ConcertsController < ApplicationController
   before_action :set_concert, only: [:show, :edit, :update, :destroy]
 
   def search
-    @response = HTTParty.get('http://www.nvivo.es/api/request.php?api_key=8d2007934293df8cbc2abe6192ee0f1b&method=artist.getEvents&artist=Rihanna&country_iso=us&format=json')
-    @json = JSON.parse(@response.body).with_indifferent_access
-
-    @json['response']['gigs'].each do |item|
-      p item['name']
-      p item['venue']['name']
-      p item['venue']['location']['city']
-    end
+    # @response = HTTParty.get('http://www.nvivo.es/api/request.php?api_key=8d2007934293df8cbc2abe6192ee0f1b&method=artist.getEvents&artist=Rihanna&country_iso=us&format=json')
+    # @json = JSON.parse(@response.body).with_indifferent_access
+    #
+    # @json['response']['gigs'].each do |item|
+    #   p item['name']
+    #   p item['venue']['name']
+    #   p item['venue']['location']['city']
+    # end
   end
 
   # GET /concerts

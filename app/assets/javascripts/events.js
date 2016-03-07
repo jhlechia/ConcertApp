d39d72e1618441d496cf2bdf2d9d151b076dc914
jmils <jamie.milanese@gmail.com>
2016-03-06 20:40:18 -0500

    Add geocoding service from google api, fix div container to contain map, adjust map height to 88%, not perfect but ok for now

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -29,7 +29,59 @@ if (navigator.geolocation) {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  var geocoder = new google.maps.Geocoder;
+
+  document.getElementById('submit').addEventListener('click', function() {
+  geocodeAddress(geocoder, map);
+});
+  // Reverse Geocode
+  // var infowindow = new google.maps.InfoWindow;
+
+// Reverse Geocode
+  // document.getElementById('submit').addEventListener('click', function() {
+  //   geocodeLatLng(geocoder, map, infowindow);
+  // });
 }
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// Reverse Geocode (take lat long, convert to)
+// function geocodeLatLng(geocoder, map, infowindow) {
+//   var input = document.getElementById('latlng').value;
+//   var latlngStr = input.split(',', 2);
+//   var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+//   geocoder.geocode({'location': latlng}, function(results, status) {
+//     if (status === google.maps.GeocoderStatus.OK) {
+//       if (results[1]) {
+//         map.setZoom(11);
+//         var marker = new google.maps.Marker({
+//           position: latlng,
+//           map: map
+//         });
+//         infowindow.setContent(results[1].formatted_address);
+//         infowindow.open(map, marker);
+//       } else {
+//         window.alert('No results found');
+//       }
+//     } else {
+//       window.alert('Geocoder failed due to: ' + status);
+//     }
+//   });
+// }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);

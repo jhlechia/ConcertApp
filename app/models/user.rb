ad01f4f1189f5685c15479796f9ab8af04a51734
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-06 20:38:48 -0500

    Added Logout button and functionality. Created a remember me check box

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def authenticated?(remember_token)
+  return false if remember_digest.nil?
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end

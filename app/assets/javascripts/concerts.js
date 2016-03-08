d4761217623953ba3aea1886b238b0505c0c6770
marcemc <marcemc@yahoo.com>
2016-03-07 21:27:39 -0500

    add 'search.js.erb' and finished ajax/api search

--- app/assets/javascripts/concerts.js
+++ app/assets/javascripts/concerts.js
@@ -1,22 +1,33 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-var init_stock_lookup;
-init_stock_lookup = function(){
-  $('#stock-lookup-form').on('ajax:before',function(){
-    show_spinner();
-  });
-
-  $('#stock-lookup-form').on('ajax:after',function(){
-    hide_spinner();
-  });
-
-}
-
-
-$(document).ready(function(){
-
-  init_stock_lookup();
-
-
-})
+// var init_concert_lookup;
+// init_concert_lookup = function(){
+//   $('#concert-lookup-form').on('ajax:before',function(){
+//     show_spinner();
+//   });
+//
+//   $('#concert-lookup-form').on('ajax:after',function(){
+//     hide_spinner();
+//   });
+//
+//   $('#concert-lookup-form').on('ajax:success', function(event, data, status){
+//     $('#concert-lookup').replaceWith(data);
+//     init_stock_lookup();
+//   });
+//
+//   $('#concert-lookup-form').on('ajax:error', function(event,xhr,statu,error){
+//     hide_spinner();
+//     $('#concert-lookup-results').replaceWith(' ');
+//     $('#concert-lookup-errors').replaceWith('Sorry, no events scheduled for that artist.');
+//
+//   });
+// }
+//
+//
+// $(document).ready(function(){
+//
+//   init_concert_lookup();
+//
+//
+// })

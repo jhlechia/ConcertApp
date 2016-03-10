6a771d3353ed443156c81af1d7affe6c904a9a87
jmils <jamie.milanese@gmail.com>
2016-03-10 14:51:45 -0500

    Replace map marker icon

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -66,10 +66,14 @@ function geocodeAddress(geocoder, resultsMap) {
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       resultsMap.setCenter(results[0].geometry.location);
+      var markerPick = '/assets/marker_40px.png';
       var marker = new google.maps.Marker({
         map: resultsMap,
-        position: results[0].geometry.location
+        position: results[0].geometry.location,
+        icon: markerPick
       });
+      // marker = marker.setOptions({'opacity': 0.5});
+
       console.log("hi")
       console.log(marker['position'].lat());
       console.log(marker['position'].lng());

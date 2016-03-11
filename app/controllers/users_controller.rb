7791d796a35d107ec3a2e59714d4e646a1e3b7df
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-10 19:44:21 -0500

    Added user profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password)
+      params.require(:user).permit(:name, :username, :email, :password, :image)
     end
 end

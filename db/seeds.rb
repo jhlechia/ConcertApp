39ff8b8426f51558b9bf8bd437f2973e6e185ac3
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-03-13 22:04:56 -0400

    Changed seed file and formated index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,53 +8,46 @@
 locations = ['26.138,-80.187', '26.108,-80.157', '26.158,-80.147', '26.168,-80.167', '26.144907,-80.130593', '26.115,-80.127', '26.112002,-80.209643']
 
 
-(1..10).each do |u|
-  User.create(name: "User#{u}", username: "username#{u}", email: "user#{u}@email.com", password: "password")
-end
+  User.create(name: "Bradley Cooper", username: "bcoops10", email: "bcoops@gmail.com", password: "password")
+  User.create(name: "Ruby Rose", username: "rubyroo", email: "rrose@gmail.com", password: "password")
 
-(1..5).each do |c|
-  Concert.create(artist: "Artist#{c}", venue: "place#{c}", date: DateTime.now, user_id: 1)
-end
 
-(6..10).each do |c|
-  Concert.create(artist: "Artist#{c}", venue: "place#{c}", date: DateTime.now, user_id: 2)
-end
+  Concert.create(artist: "Vance Joy", venue: "The Fillmore Miami Beach", date: '2016-03-19T11;30;00+00:00', user_id: 1)
+  # Concert.create(artist: "Artist#{c}", venue: "place#{c}", date: DateTime.now, user_id: 1)
+
 
 (0..6).each do |location|
   Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 1)
 end
 
-(0..6).each do |location|
-  Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 2)
-end
 
-(0..6).each do |location|
-  Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 3)
-end
-
-(0..6).each do |location|
-  Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 4)
-end
+# (0..6).each do |location|
+#   Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 3)
+# end
+#
+# (0..6).each do |location|
+#   Event.create(is_carpool: true, location: "#{locations[location]}", concert_id: 4)
+# end
 
 
-(11..15).each do |meet|
-  Event.create(is_meetup: true, time:DateTime.now, description: "this is the description for event No #{meet}", concert_id: 1)
-end
+# (11..15).each do |meet|
+#   Event.create(is_meetup: true, time:DateTime.now, description: "this is the description for event No #{meet}", concert_id: 1)
+# end
 
-(16..20).each do |meet|
-  Event.create(is_meetup: true, time:DateTime.now, description: "this is the description for event No #{meet}", concert_id: 2)
-end
+# (16..20).each do |meet|
+#   Event.create(is_meetup: true, time:DateTime.now, description: "this is the description for event No #{meet}", concert_id: 2)
+# end
 
-Message.create(body:"blah", event_id:1)
-Message.create(body:"blah", event_id:1)
-Message.create(body:"blah", event_id:2)
-Message.create(body:"blah", event_id:2)
-Message.create(body:"blah", event_id:3)
-Message.create(body:"blah", event_id:3)
-
-Message.create(body:"blah", event_id:16)
-Message.create(body:"blah", event_id:16)
-Message.create(body:"blah", event_id:17)
-Message.create(body:"blah", event_id:17)
-Message.create(body:"blah", event_id:18)
-Message.create(body:"blah", event_id:18)
+# Message.create(body:"blah", event_id:1)
+# Message.create(body:"blah", event_id:1)
+# Message.create(body:"blah", event_id:2)
+# Message.create(body:"blah", event_id:2)
+# Message.create(body:"blah", event_id:3)
+# Message.create(body:"blah", event_id:3)
+#
+# Message.create(body:"blah", event_id:16)
+# Message.create(body:"blah", event_id:16)
+# Message.create(body:"blah", event_id:17)
+# Message.create(body:"blah", event_id:17)
+# Message.create(body:"blah", event_id:18)
+# Message.create(body:"blah", event_id:18)
